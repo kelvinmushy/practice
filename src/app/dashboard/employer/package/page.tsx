6ff719f42a1950dcaf2fa796e4c7d1de61c51664
kelvin cosmas <kevo93mushy@gmail.com>
2024-04-01 23:47:09 +0300

    restructure

--- src/app/dashboard/employer/package/page.tsx
+++ src/app/dashboard/employer/package/page.tsx
@@ -3,9 +3,194 @@ import React from "react";
 const Package=()=>{
 
     return (
-        <p>All applicant</p>
+        <div>
+            <div className="flex flex-row w-full md:p-20">
+          
+                     <div className="table-outer">
+                        <table className="default-table manage-job-table">
+                           <thead>
+                              <tr>
+                                 <th>#</th>
+                                 <th>Transaction id</th>
+                                 <th>Package</th>
+                                 <th>Expiry</th>
+                                 <th>Total Jobs/CVs</th>
+                                 <th>Used</th>
+                                 <th>Remaining</th>
+                                 <th>Status</th>
+                              </tr>
+                           </thead>
+                           <tbody>
+                              <tr>
+                                 <td>1</td>
+                                 <td className="trans-id">#593677663</td>
+                                 <td className="package"><a href="#">Super CV Pack</a></td>
+                                 <td className="expiry">Jan 11, 2021</td>
+                                 <td className="total-jobs">50</td>
+                                 <td className="used">8</td>
+                                 <td className="remaining">42</td>
+                                 <td className="status">Active</td>
+                              </tr>
+                              <tr>
+                                 <td>2</td>
+                                 <td className="trans-id">#593677663</td>
+                                 <td className="package"><a href="#">Gold Jobs package</a></td>
+                                 <td className="expiry">Jan 11, 2021</td>
+                                 <td className="total-jobs">50</td>
+                                 <td className="used">8</td>
+                                 <td className="remaining">42</td>
+                                 <td className="status">Active</td>
+                              </tr>
+                              <tr>
+                                 <td>3</td>
+                                 <td className="trans-id">#593677663</td>
+                                 <td className="package"><a href="#">Silver Jobs package</a></td>
+                                 <td className="expiry">Jan 11, 2021</td>
+                                 <td className="total-jobs">50</td>
+                                 <td className="used">8</td>
+                                 <td className="remaining">42</td>
+                                 <td className="status">Active</td>
+                              </tr>
+                              <tr>
+                                 <td>4</td>
+                                 <td className="trans-id">#593677663</td>
+                                 <td className="package"><a href="#">Super CV Pack</a></td>
+                                 <td className="expiry">Jan 11, 2021</td>
+                                 <td className="total-jobs">50</td>
+                                 <td className="used">8</td>
+                                 <td className="remaining">42</td>
+                                 <td className="status">Active</td>
+                              </tr>
+                              <tr>
+                                 <td>5</td>
+                                 <td className="trans-id">#593677663</td>
+                                 <td className="package"><a href="#">Gold Jobs package</a></td>
+                                 <td className="expiry">Jan 11, 2021</td>
+                                 <td className="total-jobs">50</td>
+                                 <td className="used">8</td>
+                                 <td className="remaining">42</td>
+                                 <td className="status">Active</td>
+                              </tr>
+                              <tr>
+                                 <td>6</td>
+                                 <td className="trans-id">#593677663</td>
+                                 <td className="package"><a href="#">Silver Jobs package</a></td>
+                                 <td className="expiry">Jan 11, 2021</td>
+                                 <td className="total-jobs">50</td>
+                                 <td className="used">8</td>
+                                 <td className="remaining">42</td>
+                                 <td className="status">Active</td>
+                              </tr>
+                           </tbody>
+                        </table>
+                     </div>
+                  </div>
+                  </div>
+
+     
     )
 
 }
 
 export default  Package;
+
+{/* <div className="flex p-20">
+   <div className="dashboard-outer px-20">
+      <div className="upper-title-box">
+         <h3>Packages!</h3>
+         <div className="text">Ready to jump back in?</div>
+      </div>
+      <div className="mb-4 ms-0 show-1023"><button type="button" className="theme-btn toggle-filters"><span className="flaticon-menu-1"></span> Menu</button></div>
+      <div className="row">
+         <div className="col-lg-12">
+            <div className="ls-widget">
+               <div className="tabs-box">
+                  <div className="widget-title">
+                     <h4>My Packages</h4>
+                  </div>
+                  <div className="widget-content">
+                     <div className="table-outer">
+                        <table className="default-table manage-job-table">
+                           <thead>
+                              <tr>
+                                 <th>#</th>
+                                 <th>Transaction id</th>
+                                 <th>Package</th>
+                                 <th>Expiry</th>
+                                 <th>Total Jobs/CVs</th>
+                                 <th>Used</th>
+                                 <th>Remaining</th>
+                                 <th>Status</th>
+                              </tr>
+                           </thead>
+                           <tbody>
+                              <tr>
+                                 <td>1</td>
+                                 <td className="trans-id">#593677663</td>
+                                 <td className="package"><a href="#">Super CV Pack</a></td>
+                                 <td className="expiry">Jan 11, 2021</td>
+                                 <td className="total-jobs">50</td>
+                                 <td className="used">8</td>
+                                 <td className="remaining">42</td>
+                                 <td className="status">Active</td>
+                              </tr>
+                              <tr>
+                                 <td>2</td>
+                                 <td className="trans-id">#593677663</td>
+                                 <td className="package"><a href="#">Gold Jobs package</a></td>
+                                 <td className="expiry">Jan 11, 2021</td>
+                                 <td className="total-jobs">50</td>
+                                 <td className="used">8</td>
+                                 <td className="remaining">42</td>
+                                 <td className="status">Active</td>
+                              </tr>
+                              <tr>
+                                 <td>3</td>
+                                 <td className="trans-id">#593677663</td>
+                                 <td className="package"><a href="#">Silver Jobs package</a></td>
+                                 <td className="expiry">Jan 11, 2021</td>
+                                 <td className="total-jobs">50</td>
+                                 <td className="used">8</td>
+                                 <td className="remaining">42</td>
+                                 <td className="status">Active</td>
+                              </tr>
+                              <tr>
+                                 <td>4</td>
+                                 <td className="trans-id">#593677663</td>
+                                 <td className="package"><a href="#">Super CV Pack</a></td>
+                                 <td className="expiry">Jan 11, 2021</td>
+                                 <td className="total-jobs">50</td>
+                                 <td className="used">8</td>
+                                 <td className="remaining">42</td>
+                                 <td className="status">Active</td>
+                              </tr>
+                              <tr>
+                                 <td>5</td>
+                                 <td className="trans-id">#593677663</td>
+                                 <td className="package"><a href="#">Gold Jobs package</a></td>
+                                 <td className="expiry">Jan 11, 2021</td>
+                                 <td className="total-jobs">50</td>
+                                 <td className="used">8</td>
+                                 <td className="remaining">42</td>
+                                 <td className="status">Active</td>
+                              </tr>
+                              <tr>
+                                 <td>6</td>
+                                 <td className="trans-id">#593677663</td>
+                                 <td className="package"><a href="#">Silver Jobs package</a></td>
+                                 <td className="expiry">Jan 11, 2021</td>
+                                 <td className="total-jobs">50</td>
+                                 <td className="used">8</td>
+                                 <td className="remaining">42</td>
+                                 <td className="status">Active</td>
+                              </tr>
+                           </tbody>
+                        </table>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+        </div> */}
--- src/app/dashboard/employer/password/page.tsx
+++ src/app/dashboard/employer/password/page.tsx
@@ -3,19 +3,9 @@ import React from "react";
 const Password=()=>{
 
     return (
-        <section className="user-dashboard  px-10">
-       <div className="dashboard-outer ">
-      <div className="upper-title-box">
-         <h3>Change Password!</h3>
-         <div className="text">Ready to jump back in?</div>
-      </div>
-      <div className="mb-4 ms-0 show-1023"><button type="button" className="theme-btn toggle-filters"><span className="flaticon-menu-1"></span> Menu</button></div>
-      <div className="ls-widget">
-         <div className="widget-title">
-            <h4>Change Password</h4>
-         </div>
-         <div className="widget-content">
-            <form className="default-form">
+       
+           <div className=" flex flex-row w-full p-20 content-center ">
+             <form className="default-form ">
                <div className="row">
                   <div className="form-group col-lg-7 col-md-12">
                     <label>Old Password </label>
@@ -33,10 +23,8 @@ const Password=()=>{
                         </div>
                </div>
             </form>
-         </div>
-      </div>
-   </div>
-</section>
+           </div>
+       
     )
 
 }
